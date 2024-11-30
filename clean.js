f8c0f216d6c84754820d2f4516fdc3203cea2a33
arafatah <arafathowlader890@gmail.com>
2024-11-30 12:59:36 +0600

    clean code

--- clean.js
+++ clean.js
@@ -1,4 +1,6 @@
-const budget = [
+'use strict';
+
+const budget = Object.freeze([
   { value: 250, description: 'Sold old TV 📺', user: 'jonas' },
   { value: -45, description: 'Groceries 🥑', user: 'jonas' },
   { value: 3500, description: 'Monthly salary 👩‍💻', user: 'jonas' },
@@ -7,29 +9,34 @@ const budget = [
   { value: -20, description: 'Candy 🍭', user: 'matilda' },
   { value: -125, description: 'Toys 🚂', user: 'matilda' },
   { value: -1800, description: 'New Laptop 💻', user: 'jonas' },
-];
+]);
 
-const spendingLimits = {
+budget[0].value = 100;
+// This object now is immutable, it can't be changed or declared new properties
+// Object.freeze is not deep freeze, it will freeze only first level.
+const spendingLimits = Object.freeze({
   jonas: 1500,
   matilda: 100,
-};
+});
+// spendingLimits.joy = 200;
+// const limit = spendingLimits[user] ? spendingLimits[user] : 0;
+// const limit = spendingLimits[user] || 0;
+// const limit = getLimit(user);
+console.log(spendingLimits);
 
 const getLimit = user => spendingLimits?.[user] ?? 0;
 
-const addExpense = function (value, description, user = 'jonas') {
-  user = user.toLowerCase();
-
-  // const limit = spendingLimits[user] ? spendingLimits[user] : 0;
-  // const limit = spendingLimits[user] || 0;
-  // const limit = getLimit(user);
+const addExpense = function (state, limit, value, description, user = 'jonas') {
+  const cleanUser = user.toLowerCase();
 
   if (value <= getLimit(user)) {
     budget.push({ value: -value, description, user });
   }
 };
-addExpense(10, 'Pizza 🍕');
-addExpense(100, 'Going to movies 🍿', 'Matilda');
-addExpense(200, 'Stuff', 'Jay');
+
+addExpense(budget, spendingLimits, 10, 'Pizza 🍕');
+addExpense(budget, spendingLimits, 100, 'Going to movies 🍿', 'Matilda');
+addExpense(budget, spendingLimits, 200, 'Stuff', 'Jay');
 
 const checkExpenses = function () {
   for (const entry of budget) {
@@ -58,4 +65,3 @@ const logBigExpenses = function (bigLimit) {
 console.log(budget);
 
 logBigExpenses(100);
-
